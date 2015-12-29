bf8044ce251640b11572887db0639d5ce1f4e7ee
Steven Masfaraud <steven@masfaraud.fr>
2015-12-29 22:40:17 +0100

    Forgot function readme

--- setup.py
+++ setup.py
@@ -7,9 +7,15 @@ Created on Tue Dec 29 13:14:53 2015
 
 from setuptools import setup
 
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
 setup(name='bms',
       version='0.01',
       description='Block-Model Simulator for python',
+      long_description=readme(),
+      keywords='block model simulation simulator time',
       url='http://github.com/masfaraud/BMSpy',
       author='Steven Masfaraud',
       author_email='steven@masfaraud.fr',
