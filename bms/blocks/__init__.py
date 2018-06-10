d8a28b7232eb9a42f3ffcc5220facde95f63c6d8
Érico Rolim <erico.erc@gmail.com>
2018-06-10 02:21:57 -0300

    Update __init__.py files.
    Makes it possible to import blocks using only bms.blocks and signals using only bms.signals.

--- bms/blocks/__init__.py
+++ bms/blocks/__init__.py
@@ -1,1 +1,5 @@
 # -*- coding: utf-8 -*-
+
+from bms.blocks.continuous import *
+from bms.blocks.nonlinear import *
+from bms.blocks.simplified_blocks import *
--- bms/signals/__init__.py
+++ bms/signals/__init__.py
@@ -1,1 +1,5 @@
 # -*- coding: utf-8 -*-
+
+from bms.signals.functions import *
+from bms.signals.simplified_signals import *
+from bms.signals.wltp import *
