e8657a3aec2c1b3f23ddcdbf7baae6af126d1ae0
Steven Masfaraud <steven@masfaraud.fr>
2018-10-07 17:50:41 +0200

    Enhancing setup.py

--- setup.py
+++ setup.py
@@ -12,27 +12,17 @@ def readme():
         return f.read()
 
 
-def version_scheme(version):
-    return '.'.join([str(i) for i in version.tag._key[1]])
-
-
-def local_scheme(version):
-    return ''
-
-
 setup(name='bms',
-      use_scm_version={
-          'version_scheme': version_scheme,
-          'local_scheme': local_scheme},
-      setup_requires=['setuptools_scm'],
-      description='Block-Model Simulator for python',
-      long_description=readme(),
-      keywords='block model simulation simulator time',
-      url='http://github.com/masfaraud/BMSpy/wiki/',
-      author='Steven Masfaraud',
-      author_email='steven@masfaraud.fr',
-      license='Creative Commons Attribution-Share Alike license',
-      packages=['bms', 'bms.blocks', 'bms.signals', 'bms.physical'],
-      package_dir={'bms': 'bms'},
-      install_requires=['numpy', 'matplotlib>=2.0', 'networkx', 'dill'],
-      classifiers=['Topic :: Scientific/Engineering', 'Development Status :: 3 - Alpha'])
+      use_scm_version = True,
+      setup_requires = ['setuptools_scm'],
+      description = 'Block-Model Simulator for python',
+      long_description = readme(),
+      keywords = 'block model simulation simulator time',
+      url = 'http://github.com/masfaraud/BMSpy/wiki/',
+      author = 'Steven Masfaraud',
+      author_email = 'steven@masfaraud.fr',
+      license = 'Creative Commons Attribution-Share Alike license',
+      packages = ['bms', 'bms.blocks', 'bms.signals', 'bms.physical'],
+      package_dir = {'bms': 'bms'},
+      install_requires = ['numpy', 'matplotlib>=2.0', 'networkx', 'dill'],
+      classifiers = ['Topic :: Scientific/Engineering', 'Development Status :: 3 - Alpha'])
