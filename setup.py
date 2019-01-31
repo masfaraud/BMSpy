4a4b5b148e19e62cdd55d76ca3d8bd685c675bf7
Steven Masfaraud <masfaraud@dessia.tech>
2019-01-31 11:44:14 +0100

    Fixing https://github.com/masfaraud/BMSpy/issues/16

--- setup.py
+++ setup.py
@@ -5,16 +5,54 @@ Setup install script for BMS
 """
 
 from setuptools import setup
-
+from os.path import dirname, isdir, join
+import re
+from subprocess import CalledProcessError, check_output
 
 def readme():
     with open('README.rst') as f:
         return f.read()
+    
+tag_re = re.compile(r'\btag: %s([0-9][^,]*)\b')
+version_re = re.compile('^Version: (.+)$', re.M)
 
+def get_version():
+    # Return the version if it has been injected into the file by git-archive
+    version = tag_re.search('$Format:%D$')
+    if version:
+        return version.group(1)
+
+    d = dirname(__file__)
+    
+    if isdir(join(d, '.git')):
+        cmd = 'git describe --tags  --dirty'
+        try:
+            version = check_output(cmd.split()).decode().strip()[:]
+        except CalledProcessError:
+            raise RuntimeError('Unable to get version number from git tags')
+        if version[0]=='v':
+            version = version[1:]
+#        print(version)
+        # PEP 440 compatibility
+        if '-' in version:
+            if version.endswith('-dirty'):
+                version = '.dev'.join(version.split('-')[:-1][:2])+'-dirty'
+        else:
+            version = '.dev'.join(version.split('-')[:2])
+
+    else:
+        # Extract the version from the PKG-INFO file.
+        with open(join(d, 'PKG-INFO')) as f:
+            version = version_re.search(f.read()).group(1)
+            
+#    # Writing to file
+#    with open('powertransmission/version.py', 'w+') as vf:
+#        vf.write("# -*- coding: utf-8 -*-\nversion = '{}'".format(version))
+                 
+    return version
 
 setup(name='bms',
-      use_scm_version = True,
-      setup_requires = ['setuptools_scm'],
+      version = get_version(),
       description = 'Block-Model Simulator for python',
       long_description = readme(),
       keywords = 'block model simulation simulator time',
