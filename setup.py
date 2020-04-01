7f2b7c88712f498702a81a188352d226d8e24078
Steven Masfaraud <masfaraud@dessia.tech>
2020-04-01 13:04:44 +0200

    Adding drone

--- setup.py
+++ setup.py
@@ -16,6 +16,61 @@ def readme():
 tag_re = re.compile(r'\btag: %s([0-9][^,]*)\b')
 version_re = re.compile('^Version: (.+)$', re.M)
 
+def version_from_git_describe(version):
+    if version[0]=='v':
+            version = version[1:]
+            
+    # PEP 440 compatibility
+    number_commits_ahead = 0
+    if '-' in version:
+        version, number_commits_ahead, commit_hash = version.split('-')
+        number_commits_ahead = int(number_commits_ahead)
+        
+    # print('number_commits_ahead', number_commits_ahead)
+    
+    split_versions = version.split('.')
+    if 'post' in split_versions[-1]:
+        suffix = split_versions[-1]
+        split_versions = split_versions[:-1]
+    else:
+        suffix = None
+    
+    for pre_release_segment in ['a', 'b', 'rc']:
+        if pre_release_segment in split_versions[-1]:
+            if number_commits_ahead > 0:
+                split_versions[-1] = str(split_versions[-1].split(pre_release_segment)[0])
+                if len(split_versions) == 2:
+                    split_versions.append('0')
+                if len(split_versions) == 1:
+                    split_versions.extend(['0', '0'])
+        
+                split_versions[-1] = str(int(split_versions[-1])+1)
+                future_version = '.'.join(split_versions)
+                return '{}.dev{}'.format(future_version, number_commits_ahead)
+            else:
+                return '.'.join(split_versions)
+            
+    if number_commits_ahead > 0:
+        if len(split_versions) == 2:
+            split_versions.append('0')
+        if len(split_versions) == 1:
+            split_versions.extend(['0', '0'])
+        split_versions[-1] = str(int(split_versions[-1])+1)
+        split_versions = '.'.join(split_versions)
+        return '{}.dev{}'.format(split_versions, number_commits_ahead)
+    else:
+        if suffix is not None:
+            split_versions.append(suffix)
+
+        return '.'.join(split_versions)
+    
+# Just testing if get_version works well
+assert version_from_git_describe('v0.1.7.post2') == '0.1.7.post2'
+assert version_from_git_describe('v0.0.1-25-gaf0bf53') == '0.0.2.dev25'
+assert version_from_git_describe('v0.1-15-zsdgaz') == '0.1.1.dev15'
+assert version_from_git_describe('v1') == '1'
+assert version_from_git_describe('v1-3-aqsfjbo') == '1.0.1.dev3'
+    
 def get_version():
     # Return the version if it has been injected into the file by git-archive
     version = tag_re.search('$Format:%D$')
@@ -25,30 +80,20 @@ def get_version():
     d = dirname(__file__)
     
     if isdir(join(d, '.git')):
-        cmd = 'git describe --tags  --dirty'
+        cmd = 'git describe --tags'
         try:
             version = check_output(cmd.split()).decode().strip()[:]
+            
         except CalledProcessError:
             raise RuntimeError('Unable to get version number from git tags')
-        if version[0]=='v':
-            version = version[1:]
-#        print(version)
-        # PEP 440 compatibility
-        if '-' in version:
-            if version.endswith('-dirty'):
-                version = '.dev'.join(version.split('-')[:-1][:2])+'-dirty'
-        else:
-            version = '.dev'.join(version.split('-')[:2])
-
+        
+        return version_from_git_describe(version)
     else:
         # Extract the version from the PKG-INFO file.
         with open(join(d, 'PKG-INFO')) as f:
             version = version_re.search(f.read()).group(1)
             
-#    # Writing to file
-#    with open('powertransmission/version.py', 'w+') as vf:
-#        vf.write("# -*- coding: utf-8 -*-\nversion = '{}'".format(version))
-                 
+    # print('version', version)
     return version
 
 setup(name='bms',
